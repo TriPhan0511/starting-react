9709191f2a4a8532a614a13774daa67fb02c3902
Tri Phan <phanphutri@gmail.com>
2022-12-03 20:10:47 +0700

    Updated

--- src/samples/my-app-redux/MyApp.js
+++ src/samples/my-app-redux/MyApp.js
@@ -2,44 +2,38 @@ import React, { useEffect } from 'react'
 import { Provider, useDispatch } from 'react-redux'
 import store from './store'
 
-import {
-	PageContainer,
-	TwoColumnLayout,
-	Title,
-} from './components/StyledComponents'
+import { PageContainer, TwoColumnLayout, Title } from './components/StyledComponents'
 
 import PokemonTable from './components/PokemonTable'
-import PokemonInfo from './components/PokemonInfo'
 
 function MyApp() {
-	const dispatch = useDispatch()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    async function fetchPokemons() {
+      const resp = await fetch('/starting-react/pokemon.json')
+      const data = await resp.json()
+      dispatch({
+        type: 'SET_POKEMONS',
+        payload: data,
+      })
+    }
+    fetchPokemons()
+  })
 
-	useEffect(() => {
-		async function fetchPokemons() {
-			const resp = await fetch('/starting-react/pokemon.json')
-			const data = await resp.json()
-			dispatch({
-				type: 'SET_POKEMONS',
-				payload: data,
-			})
-		}
-		fetchPokemons()
-	})
-
-	return (
-		<PageContainer>
-			<Title>Pokemon Search</Title>
-			<TwoColumnLayout>
-				<PokemonTable />
-			</TwoColumnLayout>
-		</PageContainer>
-	)
+  return (
+    <PageContainer>
+      <Title>Pokemon Search</Title>
+      <TwoColumnLayout>
+        <PokemonTable />
+      </TwoColumnLayout>
+    </PageContainer>
+  )
 }
 
 export default () => {
-	return (
-		<Provider store={store}>
-			<MyApp />
-		</Provider>
-	)
+  return (
+    <Provider store={store}>
+      <MyApp />
+    </Provider>
+  )
 }
--- src/samples/my-app-redux/components/PokemonFilter.js
+++ src/samples/my-app-redux/components/PokemonFilter.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Input } from './StyledComponents'
 
 function PokemonFilter() {
-	const dispatch = useDispatch()
-	const filter = useSelector((state) => state.filter)
-	return (
-		<Input
-			type='text'
-			value={filter}
-			onChange={(e) => {
-				dispatch({
-					type: 'SET_FILTER',
-					payload: e.target.value,
-				})
-			}}
-		/>
-	)
+  const dispatch = useDispatch()
+  const filter = useSelector((state) => state.filter)
+  return (
+    <Input
+      type='text'
+      value={filter}
+      onChange={(e) => {
+        dispatch({
+          type: 'SET_FILTER',
+          payload: e.target.value,
+        })
+      }}
+    />
+  )
 }
 
 export default PokemonFilter
